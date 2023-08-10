7c6eb7c4e1a33d3b47f9d1abdc7c692710a87f82
Igor Ferreira <iferreira.n01@gmail.com>
2023-08-10 20:30:33 -0300

    Configura migrações para o banco de dados

---
+++ fast_zero/settings.py
@@ -0,0 +1,10 @@
+from pydantic import Field
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    DATABASE_URL: str = Field(default=...)
+
+    model_config = SettingsConfigDict(
+        env_file='.env', env_file_encoding='utf-8'
+    )
